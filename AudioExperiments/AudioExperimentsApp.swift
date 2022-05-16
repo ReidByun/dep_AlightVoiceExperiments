df9b415077b38ce93fecfd84f67e3c09494dbc9d
Changhee <temphee@gmail.com>
2022-05-16 18:57:18 +0900

    Initial Commit

---
+++ AudioExperiments/AudioExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioExperimentsApp.swift
+//  AudioExperiments
+//
+//  Created by temphee Reid on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct AudioExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
